7c9b7e5c70fa497affbe287f9760b0c79a23ac1a
favourdan <106870617+favourdan@users.noreply.github.com>
2023-02-18 02:55:41 +0100

    updated

--- src/main/java/com/example/favouriteclothingstore/controller/UserController.java
+++ src/main/java/com/example/favouriteclothingstore/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String sayHello(){
-       return "Hello world all";
+       return "Hello world my people";
     }
 
 }
